063d9d8ef836dd63f949a09e7267e40c9622e7cb
ripfag <aleksandrbistrov1@bk.ru>
2025-11-16 19:46:44 +0300

    Add files via upload

---
+++ key_words.py
@@ -0,0 +1,27 @@
+def max_number(a,b):
+    if a>b:
+        return a
+    else:
+        return b
+
+
+def empty_function():
+    pass
+
+
+def even_numbers(n):
+    for numbers in range(0,n+1, 2):
+        yield numbers
+
+
+def max_number_check():
+    assert max_number(1,1) ==1, "Числа равны!"
+    assert max_number(5,4) ==5, "Ошибка, 5>4!"
+    assert max_number(100,1000) ==1000, "Ошибка, 1000>100!"
+
+
+max_number_check()
+print("Тесты пройдены!")
+print(max_number(10,20))
+for num in even_numbers(10):
+    print(num)
